ccd743875b5de3265efd613ad09ff7cdb4bedf33
Jaikishore <jaikishore@dlearners.in>
2024-09-03 17:03:40 +0530

    dummy scene included

---
+++ Assets/Scenes/SampleScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    public void DisplayConsoleLog(){
+        Debug.Log($"BUTTON CLICKED.....!!");
+    }
+}
